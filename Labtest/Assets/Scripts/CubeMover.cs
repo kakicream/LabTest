29bb5d5053ba341af6d5b4e281148ad33c4efba5
kakicream <79087953+kakicream@users.noreply.github.com>
2024-01-18 15:07:41 +0900

    4thcommit
    4thcommit

--- Labtest/Assets/Scripts/CubeMover.cs
+++ Labtest/Assets/Scripts/CubeMover.cs
@@ -6,10 +6,18 @@ public class CubeMover : MonoBehaviour
 {
     void Update()
     {
-        transform.position += Vector3.forward * Time.deltaTime;
         if (transform.position.x >= 5.0f)
         {
             transform.position += Vector3.back * Time.deltaTime;
+        }
+        else
+        {
+            transform.position += Vector3.forward * Time.deltaTime;
+        }
+
+        if (transform.position.y >= 0.0f)
+        {
+            transform.position += Vector3.up * Time.deltaTime;
         }
     }
 }
