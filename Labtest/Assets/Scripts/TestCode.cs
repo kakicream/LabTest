7c5ade6ea889663fd74ec98cbd0ffce4ae500d85
kakicream <79087953+kakicream@users.noreply.github.com>
2024-01-18 11:00:13 +0900

    1stcommit
    tests

---
+++ Labtest/Assets/Scripts/TestCode.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Begin");
+        Debug.Log("is it working?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
